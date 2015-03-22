8b16f280c3076a4e2a8bf4a9d9c64955dce32345
tony <tony@tonys-MacBook-Pro.local>
2015-03-22 21:43:46 +0800

    初始化

---
+++ CoreData/CoreData/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CoreData
+//
+//  Created by tony on 15/3/22.
+//  Copyright (c) 2015年 tony. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CoreData/CoreData/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  CoreData
+//
+//  Created by tony on 15/3/22.
+//  Copyright (c) 2015年 tony. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
